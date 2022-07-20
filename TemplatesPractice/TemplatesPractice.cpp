8c5f9f6ba3203c84329a80a34db1c7221dcda420
Brett Wuebben <62609232+Brettw1419@users.noreply.github.com>
2022-07-20 17:41:12 -0400

    Finished Unit 8.2B

--- TemplatesPractice/TemplatesPractice.cpp
+++ TemplatesPractice/TemplatesPractice.cpp
@@ -3,9 +3,41 @@
 // July 20, 2022
 
 #include <iostream>
+using namespace std;
+
+template <typename N>
+N half(N number);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double a = 7.0;
+    float b = 5.0f;
+    int c = 3;
+
+    cout << half(a) << endl;
+    cout << half(b) << endl;
+    cout << half(c) << endl;
 }
 
+template <typename N>
+N half(N number) {
+    //Checks for integer type
+    if (typeid(number) == typeid(int)) {
+        double temp = (double)number / (double)2;
+        int out;
+        //Round up if target >= x.5
+        if (temp >= floor(temp) + .5) {
+            out = ceil(temp);
+            return out;
+        }
+        //Round down/Truncate
+        else {
+            out = floor(temp);
+            return out;
+        }
+    }  
+    //Checks for floating point numbers
+    else if (typeid(number) == typeid(float) || typeid(number) == typeid(double)) {
+        return number / 2;
+    }
+}
